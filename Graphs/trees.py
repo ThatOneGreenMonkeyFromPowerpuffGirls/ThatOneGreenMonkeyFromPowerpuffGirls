47d3f4ae959b21943147ef4e48bbde83ef76e5a3
Mojo Jojo <152896251+ThatOneGreenMonkeyFromPowerpuffGirls@users.noreply.github.com>
2024-04-16 09:10:41 +0000

    16/04/2024

---
+++ Graphs/trees.py
@@ -0,0 +1,17 @@
+left = 1
+data = 2
+right = 3
+position = 0
+tree = [
+    [0, 1, "one", 4],
+    [1, 2, "two", 3],
+    [2, None, "four", None],
+    [3, None, "five", None],
+    [4, None, "three", None]
+]
+
+def inorderTraversal():
+    for i in range (0, len(tree)):
+
+
+print(len(tree))
